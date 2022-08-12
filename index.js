b10459ae149c304a099cf9b185dec5f265cbcae8
MohamedElk6 <54820708+MohamedElk6@users.noreply.github.com>
2022-08-12 17:56:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const json = require("./data.json");
+const json = require("/workspaces/ManyChat/data.json");
 let PORT = process.env.PORT || 3000;
 app.get("/", (req,res) => {
     res.send("Hello World")
