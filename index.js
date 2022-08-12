ab08097200af9064dafaae7036087977ba62df88
MohamedElk6 <54820708+MohamedElk6@users.noreply.github.com>
2022-08-12 18:07:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const json = require("/workspaces/ManyChat/data.json");
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 5000;
 app.get("/", (req,res) => {
     res.send("Hello World")
 });
@@ -9,6 +9,9 @@ app.get("/api", (req,res) => {
     res.send(data)
 });
 function manychatjson(json,cardnum=10) {
+    var title = "name";
+    var subtitle = "description";
+    var image_url = "image";
     num = json["products"].length;
     if (cardnum>num || cardnum == 0){
         cardnum = num
@@ -19,14 +22,14 @@ function manychatjson(json,cardnum=10) {
         manychat["content"]["messages"].push({"type": "cards","elements": [],"image_aspect_ratio": "horizontal"});
         x = i;
         for (let j = 0; j < cardnum; j++)   {
-            manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j]["name"],"subtitle": json["products"][(i*cardnum)+j]["description"],"image_url": json["products"][(i*cardnum)+j]["image"],"action_url": "https://manychat.com","buttons": []});
+            manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j][title],"subtitle": json["products"][(i*cardnum)+j][subtitle],"image_url": json["products"][(i*cardnum)+j][image_url],"action_url": "https://manychat.com","buttons": []});
         }
     }
     if (num%cardnum > 0)    {
         manychat["content"]["messages"].push({"type": "cards","elements": [],"image_aspect_ratio": "horizontal"});
         for (let i = x+1; i < x+2; i++){
             for(let j = 0; j < num%cardnum; j++)    {
-                manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j]["name"],"subtitle": json["products"][(i*cardnum)+j]["description"],"image_url": json["products"][(i*cardnum)+j]["image"],"action_url": "https://manychat.com","buttons": []});
+                manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j][title],"subtitle": json["products"][(i*cardnum)+j][subtitle],"image_url": json["products"][(i*cardnum)+j][image_url],"action_url": "https://manychat.com","buttons": []});
             }
         }
     }
