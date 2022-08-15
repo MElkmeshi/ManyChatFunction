8ee910ac3ef342e0d5f3be8dc8950b73ca00b31b
MohamedElk6 <54820708+MohamedElk6@users.noreply.github.com>
2022-08-15 07:50:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const json = require("/workspaces/ManyChat/data.json");
+const json = require("./data.json");
 let PORT = process.env.PORT || 5000;
 app.get("/", (req,res) => {
     res.send("Hello World")
@@ -9,35 +9,37 @@ app.get("/api", (req,res) => {
     res.send(data)
 });
 function manychatjson(json,cardnum=10) {
-    var title = "name";
-    var subtitle = "description";
-    var image_url = "image";
-    num = json["products"].length;
-    if (cardnum>num || cardnum == 0){
-        cardnum = num
-    }
+    if (cardnum>num || cardnum == 0)
+        cardnum = num;
+    //{if you want to edit} edit the num varibles to the length of the list you want to repart about
+    var num = json["products"].length,i;
     manychat = {"version": "v2","content": {"messages": [],"actions": [],"quick_replies": []}};
-    x = 1;
-    for (let i = 0; i < Math.floor(num/cardnum); i++)   {
+    for (i = 0; i < Math.floor(num/cardnum); i++)   {
         manychat["content"]["messages"].push({"type": "cards","elements": [],"image_aspect_ratio": "horizontal"});
-        x = i;
         for (let j = 0; j < cardnum; j++)   {
-            manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j][title],"subtitle": json["products"][(i*cardnum)+j][subtitle],"image_url": json["products"][(i*cardnum)+j][image_url],"action_url": "https://manychat.com","buttons": []});
+            /*edit each vaible the way you want it to display
+            don't forget the other for loop*/
+            title = json["products"][(i*cardnum)+j]["name"];
+            subtitle = json["products"][(i*cardnum)+j]["description"];
+            image_url = json["products"][(i*cardnum)+j]["image"];
+            manychat["content"]["messages"][i]["elements"].push({"title": title,"subtitle": subtitle,"image_url": image_url,"action_url": "https://manychat.com","buttons": []});
         }
     }
-    if (num%cardnum > 0)    {
+    if (num%cardnum > 0)   {
         manychat["content"]["messages"].push({"type": "cards","elements": [],"image_aspect_ratio": "horizontal"});
-        for (let i = x+1; i < x+2; i++){
             for(let j = 0; j < num%cardnum; j++)    {
-                manychat["content"]["messages"][i]["elements"].push({"title": json["products"][(i*cardnum)+j][title],"subtitle": json["products"][(i*cardnum)+j][subtitle],"image_url": json["products"][(i*cardnum)+j][image_url],"action_url": "https://manychat.com","buttons": []});
+                //this one
+                title = json["products"][(i*cardnum)+j]["name"];
+                subtitle = json["products"][(i*cardnum)+j]["description"];
+                image_url = json["products"][(i*cardnum)+j]["image"];
+                manychat["content"]["messages"][i]["elements"].push({"title": title,"subtitle": subtitle,"image_url": image_url,"action_url": "https://manychat.com","buttons": []});    
             }
-        }
     }
     return manychat;
 }
-app.post("/test", (req,res) => {
+app.get("/test", (req,res) => {
     cardnum = req.headers.cardnum
-    res.send(manychatjson(json,cardnum));
+    res.send(manychatjson(json,9));
 });
 app.listen(PORT, () => {
     console.log(`listening on post ${PORT}`);
